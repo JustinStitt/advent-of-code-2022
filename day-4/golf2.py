5de76e6c77d2a666a9717ada88faa82c9eb23b4f
Justin Stitt <cpt.crixus@gmail.com>
2022-12-03 21:49:06 -0800

    add golf2-2.py

--- day-4/golf2.py
+++ day-4/golf2.py
@@ -1,13 +1,9 @@
 import sys
 
 a = sum(
-    [
-        1
-        if (a >= c and a <= d)
-        or (b >= c and b <= d)
-        or (c >= a and c <= b)
-        or (d >= a and d <= b)
-        else 0
+    1 if (len(n) + len(m)) > len(set(n) | set(m)) else 0
+    for (n, m) in [
+        (range(int(a), int(b) + 1), range(int(c), int(d) + 1))
         for (a, b, c, d) in [
             (*i.split("-"), *ii.split("-"))
             for (i, ii) in [l.split(",") for l in sys.stdin.readlines()]
