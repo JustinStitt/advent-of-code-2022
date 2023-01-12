e5a41b7f2f9e62f3b1e44902aec3d0d6607eb792
Justin Stitt <cpt.crixus@gmail.com>
2023-01-12 14:06:03 -0800

    2015 day 6 both parts

---
+++ 2015/day-6/solve1.py
@@ -0,0 +1,42 @@
+import sys
+import re
+from pipe import traverse
+from collections import defaultdict
+from aocdebug.debug import debug
+
+DEBUG = False
+debug = debug if DEBUG else lambda *_: 0
+
+lines = [line.rstrip() for line in sys.stdin.readlines()]
+
+OFF = 0
+ON = 1
+
+grid = [[OFF for __ in range(1000)] for _ in range(1000)]
+
+intervals = []  # (r1, c1, r2, c2)
+
+for line in lines:
+    # extract two intervals
+    fm, sm = re.findall(r"\d+,\d+", line)
+    fm = [int(x) for x in fm.split(",")]
+    sm = [int(x) for x in sm.split(",")]
+    debug(fm, sm)
+
+    # determine type of interval (turn on, turn off, toggle)
+    turn_on = "on" in line
+    turn_off = "off" in line
+    turn_toggle = "toggle" in line
+
+    for r in range(fm[0], sm[0] + 1):
+        for c in range(fm[1], sm[1] + 1):
+            if turn_on:
+                grid[r][c] = ON
+            elif turn_off:
+                grid[r][c] = OFF
+            elif turn_toggle:
+                grid[r][c] = not grid[r][c]
+
+# count number of lights turned on
+answer = sum(grid | traverse)
+print(answer)
